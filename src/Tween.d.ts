6668aa9ba69fec5277a900ca78696a98e5f28a3d
Giff Song <giff.song@gmail.com>
2016-01-02 00:24:28 +0900

    Make definition file as modules typing file

--- src/Tween.d.ts
+++ src/Tween.d.ts
@@ -3,33 +3,31 @@
 // Definitions by: sunetos <https://github.com/sunetos>, jzarnikov <https://github.com/jzarnikov>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-declare module TWEEN {
-  export var REVISION: string;
-  export function getAll(): Tween[];
-  export function removeAll(): void;
-  export function add(tween:Tween): void;
-  export function remove(tween:Tween): void;
-  export function update(time?:number): boolean;
+export var REVISION: string;
+export function getAll(): Tween[];
+export function removeAll(): void;
+export function add(tween:Tween): void;
+export function remove(tween:Tween): void;
+export function update(time?:number): boolean;
 
-  export class Tween {
-    constructor(object?:any);
-    to(properties:any, duration:number): Tween;
-    start(time?:number): Tween;
-    stop(): Tween;
-    delay(amount:number): Tween;
-    easing(easing: (k: number) => number): Tween;
-    interpolation(interpolation: (v:number[], k:number) => number): Tween;
-    chain(...tweens:Tween[]): Tween;
-    onStart(callback: (object?: any) => void): Tween;
-    onUpdate(callback: (object?: any) => void): Tween;
-    onComplete(callback: (object?: any) => void): Tween;
-    update(time: number): boolean;
-    repeat(times: number): Tween;
-    yoyo(enable: boolean): Tween;
-  }
-  export var Easing: TweenEasing;
-  export var Interpolation: TweenInterpolation;
+export class Tween {
+  constructor(object?:any);
+  to(properties:any, duration:number): Tween;
+  start(time?:number): Tween;
+  stop(): Tween;
+  delay(amount:number): Tween;
+  easing(easing: (k: number) => number): Tween;
+  interpolation(interpolation: (v:number[], k:number) => number): Tween;
+  chain(...tweens:Tween[]): Tween;
+  onStart(callback: (object?: any) => void): Tween;
+  onUpdate(callback: (object?: any) => void): Tween;
+  onComplete(callback: (object?: any) => void): Tween;
+  update(time: number): boolean;
+  repeat(times: number): Tween;
+  yoyo(enable: boolean): Tween;
 }
+export var Easing: TweenEasing;
+export var Interpolation: TweenInterpolation;
 
 interface TweenEasing {
   Linear: {
